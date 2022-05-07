8bbe65398527efa62f71b8843e6a9c60ba7e3f9b
Anastasija Selevjorstova <selevjorstovaa@gmail.com>
2022-05-07 14:27:43 +0300

    update

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+
+$( document ).ready(function() {
+    $( ".open-close-btn" ).on('click touchstart', function(e) {
+        // prevent default anchor click
+        e.preventDefault();
+        $(".overlay").toggleClass("overlay-open");
+        $("#hamburger-icon").toggleClass("hamburger-open");
+    });
+});
